bc6e3c4711fd2852105079345fd243d09d322999
Dmitry Lakhno <dmitry@vsyaco.com>
2025-11-09 16:37:59 +0300

    phpfmt 1.1.0

--- Formula/phpfmt.rb
+++ Formula/phpfmt.rb
@@ -1,8 +1,8 @@
 class Phpfmt < Formula
   desc "Zsh wrapper around phpcbf (PSR-12) to format/fix PHP files"
   homepage "https://github.com/vsyaco/phpfmt"
-  url "https://github.com/vsyaco/phpfmt/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "23aa66e0767380831a06143ce47ffe354dc46a45549905ee3850f3036c1f49a2"
+  url "https://github.com/vsyaco/phpfmt/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "6243840e6e41b98d520c69b719976f7cc9e0d19ba5d98b27ff287affe77e540b"
   license "MIT"
 
   head "https://github.com/vsyaco/phpfmt.git", branch: "main"
@@ -20,14 +20,6 @@ class Phpfmt < Formula
     <<~EOS
       phpcbf is required and is distributed via Composer:
         composer global require squizlabs/php_codesniffer
-
-      Ensure Composer global bin is on PATH, e.g. add to your shell profile:
-        export PATH="$HOME/.composer/vendor/bin:$HOME/.config/composer/vendor/bin:$PATH"
-
-      Examples:
-        phpfmt       # format staged + unstaged (modified & untracked) PHP files
-        phpfmt -a    # format all PHP files under current directory (recursively)
-        phpfmt -p    # format PHP files changed in the latest commit (HEAD)
     EOS
   end
 
